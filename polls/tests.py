b4782cb7c19565a4c70d7bb9b84c2ce6c48c3b15
Tanish Rana <tanishrana101@gmail.com>
2025-10-17 22:57:29 -0400

    Add coverage and fix flake8 configuration

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class BasicTest(TestCase):
+    def test_basic(self):
+        """Basic test to ensure test suite works"""
+        self.assertEqual(1, 1)
